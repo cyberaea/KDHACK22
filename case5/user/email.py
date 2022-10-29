51abb460b78cee681c964e8b872132c1cb4df666
Ruslan Sushko <rusak.sushko@yandex.ru>
2022-10-29 17:24:25 +0200

    email.py

--- case5/user/email.py
+++ case5/user/email.py
@@ -1,5 +1,11 @@
 from .models import User
 from datetime import datetime
+from .send_email import send_email
 
 def email():
-    pass
+    u = User()
+    date = u.date()
+    email = u.email()
+    approved = u.approved()
+    if 24 >= datetime.now() - date >= 23 and approved:
+        return send_email(email)
