3fecf52f36d259b2a28e1f0b804ad10407e571bd
ZeNik77 <nikitkazel@gmail.com>
2022-10-29 14:20:38 +0200

    User model

--- case5/user/models.py
+++ case5/user/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# дата/время название описание тип зал оборудование фио телефон email
+
+class User(models.Model):
+    date = models.DateTimeField()
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    type = models.CharField(max_length=50)
+    hall = models.IntegerField()
+    hardware = models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+    phone_number = models.IntegerField()
+    email = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
