9e2bba4d0b10f0051c475a5eebfbe3221bccf0cd
AlexanderJRKUZ <a.kuznetsov.jr@gmail.com>
2022-10-29 13:58:43 +0200

    dayn

--- case5/case5/urls.py
+++ case5/case5/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .views import index
+
 
 urlpatterns = [
+	path('', index),
 	path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('administrator/', include('administrator.urls')),
+    path('user/', include('user.urls'))
 ]
--- case5/main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from .views import index
-
-
-urlpatterns = [
-	path('', index),
-    path('administrator/', include('administrator.urls')),
-    path('user/', include('user.urls'))
-]
