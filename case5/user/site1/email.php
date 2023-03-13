ea727f45cfce89ae6ef3b834d50e4460a5f154fb
SER <104014082+SERON123@users.noreply.github.com>
2023-03-13 20:02:49 +0200

    Update email.php

--- case5/user/site1/email.php
+++ case5/user/site1/email.php
@@ -1,7 +1,7 @@
 <?php
 $message = "Line 1\r\nLine 2\r\nLine 3";
 $message = wordwrap($message, 70, "\r\n");
-if(mail('sergeyrusanov0712@gmail.com', 'My Subject', $message)){
+if(mail('domen@gmail.com', 'My Subject', $message)){
     echo "да";
 } else {
 echo "нет";
